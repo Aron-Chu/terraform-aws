3907b938e9f2ce47f298e372a2fdbce324501cf0
Aron Chu <aroncloudchu@gmail.com>
2021-07-04 19:39:47 +0000

    added dynamic blocks and security groups

--- networking/main.tf
+++ networking/main.tf
@@ -79,3 +79,25 @@ resource "aws_default_route_table" "aron_private_rt" {
     Name = "aron_private"
   }
 }
+
+resource "aws_security_group" "aron_sg" {
+  for_each    = var.security_groups
+  name        = each.value.name
+  description = each.value.description
+  vpc_id      = aws_vpc.aron_vpc.id
+  dynamic "ingress" {
+    for_each = each.value.ingress
+    content {
+      from_port   = ingress.value.from
+      to_port     = ingress.value.to
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
